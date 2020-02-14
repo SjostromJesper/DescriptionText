fd830a78cdbd8717d51ee79fdb8def50f5399517
jesper <jesper.sjostrom@live.se>
2020-02-14 10:43:10 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -7,39 +7,22 @@ import './index.css';
 import Markdown from 'markdown-to-jsx';
 
 const App = (props) => {
-    const [boxColor, setBoxColor] = useState("primary");
-    const [editorRoles, setEditorRole] = useState(props.sdk.parameters.instance.editRights);
-    const [value, setValue] = useState(props.sdk.field.getValue() || '');
+    const boxColor = props.sdk.parameters.instance.boxColor ?? 'primary';
+
+    const editorRoles = (props.sdk.parameters.instance.editRights ?? '')
+        .split(',')
+        .map(ed => ed.toLowerCase().trim());
+    const isEditor = (props.sdk.user.spaceMembership.roles ?? [])
+        .some(r => editorRoles.includes(r.name.toLowerCase()));
+
+    const [value, setValue] = useState(props.sdk.field.getValue() ?? '');
     const [edit, setEdit] = useState(false);
-    const [isEditor, setIsEditor] = useState(false);
-
-    const changeRoleValue = (value) => {
-        const er = editorRoles.toLowerCase();
-        if(er.includes(value)) {
-            setIsEditor(true);
-        }
-    };
-
-    const checkUserRole = () => {
-        props.sdk.user.spaceMembership.roles.forEach((role) => {
-            const roleName = role.name.toLowerCase();
-            changeRoleValue(roleName);
-        });
-    };
 
     useEffect(() => {
         props.sdk.window.startAutoResizer();
-        setBoxColor(props.sdk.parameters.instance.boxColor);
 
-        const res = editorRoles.split(",").map(ed => ed.toLowerCase().trim());
-        setEditorRole(res);
-        checkUserRole();
-
-        //changeEditRoleValue(props.sdk.parameters.instance.editRights);
-        //checkUserRole();
-
-        // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-        const detachExternalChangeHandler = props.sdk.field.onValueChanged(() => {});
+        const noop = () => {};
+        const detachExternalChangeHandler = props.sdk.field.onValueChanged(noop);
 
          return () => {
              if (detachExternalChangeHandler) {
@@ -51,55 +34,43 @@ const App = (props) => {
     const onChange = e => {
         const value = e.currentTarget.value;
         setValue(value);
-
         value ? props.sdk.field.setValue(value) : props.sdk.field.removeValue();
     };
 
-    const onEditChange = () => {
-        setEdit(edit ? !edit : true);
-    };
-
-    const CurrentDescription = () => {
-        return (
-            <Note noteType={boxColor} className={"myNote"}>
+    const Description = () => (
+        <Note noteType={boxColor} className='myNote'>
             <Markdown
                 children={value}
                 options={{
                     createElement(type, props, children) {
-                        return (
-                            <span className="parent">
-                                {React.createElement(type, props, children)}
-                            </span>
-                        );
-                    },
+                        return React.createElement(type, props, children)
+                    }
                 }}
             />
-            </Note>
-        )
-    };
+        </Note>
+    );
 
     return (
-        <>
-            <div>
-                {isEditor ? (
-                            <Button className={"editButton"} onClick={onEditChange}>Edit</Button>) : null}
-                {(edit && isEditor) ? (<Textarea
-                        rows={8}
-                        labelText={""}
-                        id={"editArea"}
-                        testId={"editArea-test"}
-                        name={"editArea"}
-                        value={value}
-                        onChange={onChange}/>)
-                    : (<div className={"descriptionBox"}><CurrentDescription
-                        text={value}/></div>)}
-            </div>
-        </>
+        <div>
+            {isEditor && <Button className='editButton' onClick={() => setEdit(!edit)}>Edit</Button>}
+            {(edit && isEditor) ? (
+                <Textarea
+                    rows={8}
+                    labelText=''
+                    id='editArea'
+                    testId='editArea-test'
+                    name='editArea'
+                    value={value}
+                    onChange={onChange}/>
+            ) : (
+                <Description text={value}/>
+            )}
+        </div>
     );
 };
 
 init(sdk => {
-        ReactDOM.render(<App sdk={sdk}/>, document.getElementById('root'));
+    ReactDOM.render(<App sdk={sdk}/>, document.getElementById('root'));
 });
 
 /**
